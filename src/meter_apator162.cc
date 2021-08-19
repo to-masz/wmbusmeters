dc5f08a870fdebfb94fdf67327d375189b5e672f
Tomasz Gramza <tgramza@olx.pl>
2021-08-19 07:38:50 +0200

    Try custom offset for apator

--- src/meter_apator162.cc
+++ src/meter_apator162.cc
@@ -165,6 +165,9 @@ void MeterApator162::processContent(Telegram *t)
         debug("(apator162) adjusting to offset %d instead\n", o);
     }
 
+    o = 32
+    strprintf(total, "%02x%02x%02x%02x", content[o], content[o+1], content[o+2], content[o+3]);
+
     vendor_values["0413"] = { 25, DVEntry(MeasurementType::Instantaneous, 0x13, 0, 0, 0, total) };
     int offset;
     string key;
