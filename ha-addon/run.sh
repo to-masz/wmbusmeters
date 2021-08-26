f878f56965910d4c9a515707d4540506881f9da8
Andrzej <andriej@users.noreply.github.com>
2021-08-27 00:50:06 +0200

    lol

--- ha-addon/run.sh
+++ ha-addon/run.sh
@@ -10,10 +10,10 @@ MQTT_HOST=$(bashio::config "mqtt.host")
 MQTT_PORT=$(bashio::config "mqtt.port")
 MQTT_USER=$(bashio::config "mqtt.username")
 MQTT_PASSWORD=$(bashio::config "mqtt.password")
-echo ${MQTT_HOST}
-echo ${MQTT_PORT}
-echo ${MQTT_USERNAME}
-echo ${MQTT_PASSWORD}
+echo $MQTT_HOST
+echo $MQTT_PORT
+echo $MQTT_USERNAME
+echo $MQTT_PASSWORD
 
 if ! bashio::config.exists 'mqtt.host'; then MQTT_HOST=$(bashio::services mqtt "host"); fi
 if ! bashio::config.exists 'mqtt.port'; then MQTT_PORT=$(bashio::services mqtt "port"); fi
