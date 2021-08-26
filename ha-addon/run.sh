48e6ac8ecded063f8a66b79eb577b35d02d70138
Andrzej <andriej@users.noreply.github.com>
2021-08-27 00:45:59 +0200

    yet another try

--- ha-addon/run.sh
+++ ha-addon/run.sh
@@ -10,10 +10,10 @@ MQTT_HOST=$(bashio::config 'mqtt.host')
 MQTT_PORT=$(bashio::config 'mqtt.port')
 MQTT_USER=$(bashio::config 'mqtt.username')
 MQTT_PASSWORD=$(bashio::config 'mqtt.password')
-echo $(MQTT_HOST)
-echo $(MQTT_PORT)
-echo $(MQTT_USERNAME)
-echo $(MQTT_PASSWORD)
+echo ${MQTT_HOST}
+echo ${MQTT_PORT}
+echo ${MQTT_USERNAME}
+echo ${MQTT_PASSWORD}
 
 if ! bashio::config.exists 'mqtt.host'; then MQTT_HOST=$(bashio::services mqtt "host"); fi
 if ! bashio::config.exists 'mqtt.port'; then MQTT_PORT=$(bashio::services mqtt "port"); fi
@@ -27,7 +27,7 @@ cat > /wmbusmeters/mosquitto_pub.sh <<- "EOF"
 TOPIC=$1
 MESSAGE=$2
 
-/usr/bin/mosquitto_pub -h "`$MQTT_HOST`" -p "`$MQTT_PORT`" -u "`$MQTT_USER`" -P "``$MQTT_PASSWORD" -t $TOPIC -m "$MESSAGE"
+/usr/bin/mosquitto_pub -h "echo ${MQTT_HOST}" -p "${MQTT_PORT}" -u "${MQTT_USER}" -P "`${MQTT_PASSWORD}" -t $TOPIC -m "$MESSAGE"
 EOF
 chmod a+x /wmbusmeters/mosquitto_pub.sh
 
