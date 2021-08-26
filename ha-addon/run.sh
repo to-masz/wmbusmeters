65c5ac59b58a44cb9f032624615cd52b810b2485
Andrzej <andriej@users.noreply.github.com>
2021-08-27 01:28:27 +0200

    EOF

--- ha-addon/run.sh
+++ ha-addon/run.sh
@@ -28,7 +28,7 @@ echo 'MESSAGE=$2' >> /wmbusmeters/mosquitto_pub.sh
 echo "\n\n" >> /wmbusmeters/mosquitto_pub.sh
 echo -n "/usr/bin/mosquitto_pub -h \"${MQTT_HOST}\" -p ${MQTT_PORT} -u \"${MQTT_USER}\" -P \"${MQTT_PASSWORD}\"" >> /wmbusmeters/mosquitto_pub.sh
 echo '-t $TOPIC -m "$MESSAGE"' >> /wmbusmeters/mosquitto_pub.sh
-EOF
+
 chmod a+x /wmbusmeters/mosquitto_pub.sh
 
 echo "Syncing wmbusmeters configuration ..."
